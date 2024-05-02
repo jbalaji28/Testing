ae8f852708d1b7ac460d12b9fb377ef9d9d5b3ea
balaji <150216122+jbalaji28@users.noreply.github.com>
2024-05-02 14:24:50 +0530

    Add files via upload
    model

---
+++ abhibus/demo/src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+    ExtentReports extent;
+    ExtentTest test;
+    WebDriver driver;
+    String url="";
+}
+public static void before(){
+    
+}
+}
